ed9f119176d438384f32504b87ae03263ea6e8ea
Hiyas Javier <hiyas@klab.com>
2013-01-11 14:52:23 +0800

    view controller files, initial commit using source tree

---
+++ Hello/Hello/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by Celina Hiyas Javier on 1/11/13.
+//  Copyright (c) 2013 Celina Hiyas Javier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Hello/Hello/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Hello
+//
+//  Created by Celina Hiyas Javier on 1/11/13.
+//  Copyright (c) 2013 Celina Hiyas Javier. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    //added
+    NSLog(@"print hello!");
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
