d5e02f985759e6af373057ebecfd1282de2c8a16
Hiyas Javier <hiyas@klab.com>
2013-01-11 15:18:55 +0800

    add more files

---
+++ Hello/Hello/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by Celina Hiyas Javier on 1/11/13.
+//  Copyright (c) 2013 Celina Hiyas Javier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello/Hello/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello
+//
+//  Created by Celina Hiyas Javier on 1/11/13.
+//  Copyright (c) 2013 Celina Hiyas Javier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
